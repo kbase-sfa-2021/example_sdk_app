8027962abc871ad095b45ce51b0b08fe6663c7d6
bio-boris <bio-boris@github.com>
2021-10-14 10:43:10 -0500

    ok

--- lib/example_kb_sdk_app/example_kb_sdk_appImpl.py
+++ lib/example_kb_sdk_app/example_kb_sdk_appImpl.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
-#BEGIN_HEADER
+# BEGIN_HEADER
 import logging
 import os
 
 from installed_clients.KBaseReportClient import KBaseReport
-#END_HEADER
+from lib.example_kb_sdk_app.core import Core
+
+
+# END_HEADER
 
 
 class example_kb_sdk_app:
@@ -26,20 +29,19 @@ class example_kb_sdk_app:
     GIT_URL = ""
     GIT_COMMIT_HASH = ""
 
-    #BEGIN_CLASS_HEADER
-    #END_CLASS_HEADER
+    # BEGIN_CLASS_HEADER
+    # END_CLASS_HEADER
 
     # config contains contents of config file in a hash or None if it couldn't
     # be found
     def __init__(self, config):
-        #BEGIN_CONSTRUCTOR
+        # BEGIN_CONSTRUCTOR
         self.callback_url = os.environ['SDK_CALLBACK_URL']
         self.shared_folder = config['scratch']
         logging.basicConfig(format='%(created)s %(levelname)s: %(message)s',
                             level=logging.INFO)
-        #END_CONSTRUCTOR
+        # END_CONSTRUCTOR
         pass
-
 
     def run_example_kb_sdk_app(self, ctx, params):
         """
@@ -50,16 +52,34 @@ class example_kb_sdk_app:
         """
         # ctx is the context object
         # return variables are: output
-        #BEGIN run_example_kb_sdk_app
+        # BEGIN run_example_kb_sdk_app
+
+        param_1 = "str"
+        param_2 = "list"
+        param_3 = "dict"
+        param_4 = "int"
+
+        def validate(p1, p2, p3):
+            if not isinstance(str, param_1):
+                raise Exception("Wrong")
+
+            if not (4 < param_4 < 100):
+                raise Exception("Please provide range of params between 4 and 100")
+
+
+
+        core = Core(ctx)
+        output = core.do_analysis(params)
+
         report = KBaseReport(self.callback_url)
-        report_info = report.create({'report': {'objects_created':[],
+        report_info = report.create({'report': {'objects_created': [],
                                                 'text_message': params['parameter_1']},
-                                                'workspace_name': params['workspace_name']})
+                                     'workspace_name': params['workspace_name']})
         output = {
             'report_name': report_info['name'],
             'report_ref': report_info['ref'],
         }
-        #END run_example_kb_sdk_app
+        # END run_example_kb_sdk_app
 
         # At some point might do deeper type checking...
         if not isinstance(output, dict):
@@ -67,12 +87,13 @@ class example_kb_sdk_app:
                              'output is not type dict as required.')
         # return the results
         return [output]
+
     def status(self, ctx):
-        #BEGIN_STATUS
+        # BEGIN_STATUS
         returnVal = {'state': "OK",
                      'message': "",
                      'version': self.VERSION,
                      'git_url': self.GIT_URL,
                      'git_commit_hash': self.GIT_COMMIT_HASH}
-        #END_STATUS
+        # END_STATUS
         return [returnVal]
